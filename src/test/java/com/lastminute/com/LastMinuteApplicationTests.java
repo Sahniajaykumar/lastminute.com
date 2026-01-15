7a13a5c1acd47465eac12ff2df16259b75cfb889
Ajay Kumar <your-email@example.com>
2026-01-15 20:21:00 +0530

    Initial commit of LastMinute project

---
+++ src/test/java/com/lastminute/com/LastMinuteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastminute.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastMinuteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
